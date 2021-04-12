a112c629ddcbc0298650129824e86abd2ac88e23
Андрюха Едунов <opty@opty.ru>
2021-04-12 22:29:05 +0300

    chore: husky test

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -1,29 +1,56 @@
 import { styled } from 'linaria/react';
 import { memo, useCallback } from 'react';
+import { colors } from '../../constants';
 import { Field } from './Field';
 
 export const Board = memo(() => {
   const getHorizontalLineView = useCallback((isWhiteCellFirst = true) => {
-    console.log(isWhiteCellFirst);
-    let isWhite = isWhiteCellFirst;
-    return horizontalLineSymbolsArray.map((item) => {
-      isWhite = !isWhite;
+    return horizontalLineSymbolsArray.map((item, index) => {
+      const isWhite = Boolean(
+        Number(isWhiteCellFirst) - Number(index % 2 === 0)
+      );
+
       return <Field isWhite={isWhite} key={item} />;
     });
   }, []);
 
   const m = verticalLineSymbolsArray.map((item, index) => {
-    return <Row key={item}>{getHorizontalLineView(index % 2 === 0)}</Row>;
+    const name = verticalLineSymbolsArray.length - Number(item) + 1;
+    return (
+      <Row key={name}>
+        <LineName>{name}</LineName>
+        {getHorizontalLineView(index % 2 > 0)}
+        <LineName>{name}</LineName>
+      </Row>
+    );
   });
 
-  return <>{m}</>;
+  const alphabeticalLine = horizontalLineSymbolsArray.map((item) => (
+    <LineName key={item}>{item}</LineName>
+  ));
+
+  return (
+    <BoardWithSymbols>
+      <Row>
+        <LineName />
+        {alphabeticalLine}
+        <LineName />
+      </Row>
+      {m}
+      <Row>
+        <LineName />
+        {alphabeticalLine}
+        <LineName />
+      </Row>
+    </BoardWithSymbols>
+  );
 });
 Board.displayName = nameof(Board);
 
-const verticalLineSymbols = 'abcdefgh';
+const verticalLineSymbols = '12345678';
 const verticalLineSymbolsArray = verticalLineSymbols.split('');
 
-const horizontalLineSymbols = '12345678';
+const horizontalLineSymbols = 'abcdefgh';
 const horizontalLineSymbolsArray = horizontalLineSymbols.split('');
 
 type HorizontalLine = Record<string, string | null>;
@@ -39,7 +66,7 @@ const horizontalLine: HorizontalLine = horizontalLineSymbolsArray.reduce<Horizon
 
 type BoardCells = Record<string, HorizontalLine>;
 
-const boardCells: BoardCells = verticalLineSymbolsArray.reduce<BoardCells>(
+const boardCells: BoardCells = verticalLineSymbolsArray.reduce<Number>(
   (accumulator, current) => {
     accumulator[current] = { ...horizontalLine };
 
@@ -51,3 +78,25 @@ const boardCells: BoardCells = verticalLineSymbolsArray.reduce<BoardCells>(
 const Row = styled.div`
   display: flex;
 `;
+
+const LineName = styled.div`
+  width: 100px;
+  height: 100px;
+  font-size: 40px;
+  box-sizing: border-box;
+  padding: 25px 0 0 40px;
+  color: ${colors.lightMinor};
+  background-color: ${colors.darkMinor};
+  text-transform: uppercase;
+`;
+
+const BoardView = styled.div`
+  border: 1px solid ${colors.lightMain};
+  display: inline-block;
+  margin: 20px;
+`;
+
+const BoardWithSymbols = memo(({ children }) => {
+  return <BoardView>{children}</BoardView>;
+});
+BoardWithSymbols.displayName = nameof(BoardWithSymbols);

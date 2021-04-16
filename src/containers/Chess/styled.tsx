e19ee122ca7f4637ce1b255d26d4ab1206125308
Андрюха Едунов <opty@opty.ru>
2021-04-16 19:11:26 +0300

    fix: fixed board position

--- src/containers/Chess/styled.tsx
+++ src/containers/Chess/styled.tsx
@@ -4,6 +4,8 @@ import { cellSize } from '../../constants';
 export const ChessGrid = styled.div`
   margin: ${cellSize}px;
   position: absolute;
+  top: 0;
+  left: 0;
   display: grid;
   grid-template-rows: repeat(8, ${cellSize}px);
   grid-auto-flow: column;
